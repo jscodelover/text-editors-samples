e13fda12830f39d8b4b6e2c5563e4c1c19981c53
manisha1896 <manisha.4918@gmail.com>
2018-11-17 22:39:23 +0530

    inline tool

--- src/App.js
+++ src/App.js
@@ -6,10 +6,83 @@ import createHashtagPlugin from "draft-js-hashtag-plugin";
 import createMentionPlugin, {
   defaultSuggestionsFilter
 } from "draft-js-mention-plugin";
+import createInlineToolbarPlugin, {
+  Separator
+} from "draft-js-inline-toolbar-plugin";
+import {
+  ItalicButton,
+  BoldButton,
+  UnderlineButton,
+  CodeButton,
+  HeadlineOneButton,
+  HeadlineTwoButton,
+  HeadlineThreeButton,
+  UnorderedListButton,
+  OrderedListButton,
+  BlockquoteButton,
+  CodeBlockButton
+} from "draft-js-buttons";
+
 import "draft-js-emoji-plugin/lib/plugin.css";
 import "draft-js-hashtag-plugin/lib/plugin.css";
 import "draft-js-mention-plugin/lib/plugin.css";
+import "draft-js-inline-toolbar-plugin/lib/plugin.css";
 import "draft-js/dist/Draft.css";
+import "./App.css";
+
+class HeadlinesPicker extends Component {
+  componentDidMount() {
+    setTimeout(() => {
+      window.addEventListener("click", this.onWindowClick);
+    });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("click", this.onWindowClick);
+  }
+
+  onWindowClick = () =>
+    // Call `onOverrideContent` again with `undefined`
+    // so the toolbar can show its regular content again.
+    this.props.onOverrideContent(undefined);
+
+  render() {
+    const buttons = [HeadlineOneButton, HeadlineTwoButton, HeadlineThreeButton];
+    return (
+      <div>
+        {buttons.map((
+          Button,
+          i // eslint-disable-next-line
+        ) => (
+          <Button key={i} {...this.props} />
+        ))}
+      </div>
+    );
+  }
+}
+
+class HeadlinesButton extends Component {
+  // When using a click event inside overridden content, mouse down
+  // events needs to be prevented so the focus stays in the editor
+  // and the toolbar remains visible  onMouseDown = (event) => event.preventDefault()
+  onMouseDown = event => event.preventDefault();
+
+  onClick = () =>
+    // A button can call `onOverrideContent` to replace the content
+    // of the toolbar. This can be useful for displaying sub
+    // menus or requesting additional information from the user.
+    this.props.onOverrideContent(HeadlinesPicker);
+
+  render() {
+    return (
+      <div onMouseDown={this.onMouseDown} className="headlineButtonWrapper">
+        <button onClick={this.onClick} className="headlineButton">
+          H
+        </button>
+      </div>
+    );
+  }
+}
 
 const styleMap = {
   STRIKETHROUGH: {
@@ -23,11 +96,25 @@ const user = [
   { name: "rinki malhotra", id: "sdjvhbsdjvhcabvh" }
 ];
 
-// const imagePlugin = createImagePlugin();
+const inlineToolbarPlugin = createInlineToolbarPlugin({
+  structure: [
+    BoldButton,
+    ItalicButton,
+    UnderlineButton,
+    CodeButton,
+    Separator,
+    HeadlinesButton,
+    UnorderedListButton,
+    OrderedListButton,
+    BlockquoteButton,
+    CodeBlockButton
+  ]
+});
 
 const emojiPlugin = createEmojiPlugin();
 const hashtagPlugin = createHashtagPlugin();
 const mentionPlugin = createMentionPlugin();
+const { InlineToolbar } = inlineToolbarPlugin;
 
 const { EmojiSuggestions, EmojiSelect } = emojiPlugin;
 const { MentionSuggestions } = mentionPlugin;
@@ -108,7 +195,12 @@ class App extends Component {
             onChange={this.onChange}
             customStyleMap={styleMap}
             placeholder="Tell a story..."
-            plugins={[emojiPlugin, hashtagPlugin, mentionPlugin]}
+            plugins={[
+              emojiPlugin,
+              hashtagPlugin,
+              mentionPlugin,
+              inlineToolbarPlugin
+            ]}
             ref={element => {
               this.editor = element;
             }}
@@ -118,6 +210,23 @@ class App extends Component {
             onSearchChange={this.onSearchChange}
             suggestions={this.state.suggestions}
           />
+          <InlineToolbar>
+            {// may be use React.Fragment instead of div to improve perfomance after React 16
+            externalProps => (
+              <div>
+                <BoldButton {...externalProps} />
+                <ItalicButton {...externalProps} />
+                <UnderlineButton {...externalProps} />
+                <CodeButton {...externalProps} />
+                <Separator {...externalProps} />
+                <HeadlinesButton {...externalProps} />
+                <UnorderedListButton {...externalProps} />
+                <OrderedListButton {...externalProps} />
+                <BlockquoteButton {...externalProps} />
+                <CodeBlockButton {...externalProps} />
+              </div>
+            )}
+          </InlineToolbar>
         </div>
       </div>
     );
