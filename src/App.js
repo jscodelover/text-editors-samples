c1d043942c4ec44cb98d82a4e17d85aab52b9f3d
manisha1896 <manisha.4918@gmail.com>
2018-11-17 13:06:01 +0530

    basic code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Editor, EditorState, RichUtils } from "draft-js";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      editorState: EditorState.createEmpty()
+    };
+  }
+
+  onChange = editorState => {
+    this.setState({ editorState });
+  };
+
+  handleKeyCommand = command => {
+    console.log(command);
+    const newState = RichUtils.handleKeyCommand(
+      this.state.editorState,
+      command
+    );
+
+    if (newState) {
+      this.onChange(newState);
+      return "handled";
+    }
+
+    return "not-handled";
+  };
+
+  onUnderlineClick = () => {
+    this.onChange(
+      RichUtils.toggleInlineStyle(this.state.editorState, "UNDERLINE")
+    );
+  };
+
+  onToggleCode = () => {
+    this.onChange(RichUtils.toggleCode(this.state.editorState));
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div
+        style={{
+          width: "650px",
+          border: "2px solid",
+          height: "700px",
+          padding: "5px",
+          margin: "10px"
+        }}
+      >
+        <button onClick={this.onUnderlineClick}>Underline</button>
+        <button onClick={this.onToggleCode}>Code Block</button>
+        <Editor
+          editorState={this.state.editorState}
+          handleKeyCommand={this.handleKeyCommand}
+          onChange={this.onChange}
+        />
       </div>
     );
   }
